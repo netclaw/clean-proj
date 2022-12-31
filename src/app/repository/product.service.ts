3c87ef682ed99bea3a7a1f15fb1cade0a7521484
youssef-berkia <youssefberkiaa@gmail.com>
2022-12-31 16:59:03 +0100

    first c

---
+++ src/app/repository/product.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
+import { Product } from '../model/Product';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  products:Product[]=[new Product(1,"iphone","iphone de balur",6000,0,0,0,"null","null","https://rb.gy/hgf5rq",[""]),new Product(1,"iphone","iphone de balur",6000,0,0,0,"null","null","https://rb.gy/hgf5rq",[""]),new Product(1,"iphone","iphone de balur",6000,0,0,0,"null","null","https://rb.gy/hgf5rq",[""]),new Product(1,"iphone","iphone de balur",6000,0,0,0,"null","null","https://rb.gy/hgf5rq",[""]),new Product(1,"iphone","iphone de balur",6000,0,0,0,"null","null","https://rb.gy/hgf5rq",[""])]
+  products$:Observable<Product[]>=of(this.products);
+  constructor() { }
+}
